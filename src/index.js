01fade3e6462bb3e26536809131cde007adee63a
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2022-10-17 08:08:00 -0600

    try change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.status(200).send({ response: "HUGE DUB FOR THE FIRE NATION" });
+  res.status(200).send({ response: "Does this work?" });
 });
 
 export default router;
